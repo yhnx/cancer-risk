c5e9f5c7c33dc1887877d9818e692a83c8609301
Yehen Asuramuni <yehenaasuramuni@gmail.com>
2024-06-12 18:46:22 +0530

    minor edits

--- client/app.js
+++ client/app.js
@@ -10,36 +10,41 @@ function updateAgeSlider() {
   ageSlider.value = ageInput.value;
 }
 
-function calculateRisk() {
-  const age = parseFloat(document.getElementById("ageInput").value);
-  const sex = parseInt(document.getElementById("sex").value);
-  const height = parseFloat(document.getElementById("height").value);
-  const weight = parseFloat(document.getElementById("weight").value);
-  const alcohol = parseFloat(document.getElementById("alcohol").value);
-  const smoking = parseInt(document.getElementById("smoking").value);
-  const geneticRisk = parseInt(document.getElementById("geneticRisk").value);
-  const physicalActivity = parseFloat(
+function onClickedPredictRisk() {
+  console.log("Predict Risk button clicked");
+
+  var age = parseFloat(document.getElementById("ageInput").value);
+  var sex = parseInt(document.getElementById("sex").value);
+  var height = parseFloat(document.getElementById("height").value);
+  var weight = parseFloat(document.getElementById("weight").value);
+  var alcohol = parseFloat(document.getElementById("alcohol").value);
+  var smoking = parseInt(document.getElementById("smoking").value);
+  var geneticRisk = parseInt(document.getElementById("geneticRisk").value);
+  var physicalActivity = parseFloat(
     document.getElementById("physicalActivity").value
   );
-  const diabetes = parseInt(document.getElementById("diabetes").value);
-  const hypertension = parseInt(document.getElementById("hypertension").value);
+  var diabetes = parseInt(document.getElementById("diabetes").value);
+  var hypertension = parseInt(document.getElementById("hypertension").value);
 
-  // Hypothetical risk factor calculation
-  let riskFactor = 0;
-  riskFactor += age * 0.1;
-  riskFactor += sex * 1.5;
-  riskFactor += (weight / height) * 0.2;
-  riskFactor += alcohol * 0.3;
-  riskFactor += smoking * 2;
-  riskFactor += geneticRisk * 1.7;
-  riskFactor -= physicalActivity * 0.5;
-  riskFactor += diabetes * 1.8;
-  riskFactor += hypertension * 1.4;
+  var url = "/api/predict_risk";
 
-  // Display result
-  document.getElementById(
-    "result"
-  ).innerHTML = `<h3>Your Liver Cancer Risk Factor is: ${riskFactor.toFixed(
-    2
-  )}</h3>`;
+  jQuery.post(url, {
+    age: age,
+    sex: sex,
+    height: height,
+    weight: weight,
+    alcohol: alcohol,
+    smoking: smoking,
+    geneticRisk: geneticRisk,
+    physicalActivity: physicalActivity,
+    diabetes: diabetes,
+    hypertension: hypertension,
+  }),
+    function (data, status) {
+      console.log(data.risk_percentage);
+      document.getElementById(
+        "result"
+      ).innerHTML = `<h2>${data.risk_percentage}</h2>`;
+      console.log(status);
+    };
 }
