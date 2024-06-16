533819fc2837214108ec008607abea85ae42c939
Yehen Asuramuni <yehenaasuramuni@gmail.com>
2024-06-16 16:34:12 +0530

    took skilearn dependency away
    using oknyx

--- server/util.py
+++ server/util.py
@@ -1,46 +1,51 @@
 # Description: This file contains the utility functions that are used to load the saved artifacts and make predictions.
 import json 
 import pickle
+import numpy as np
 from flask import render_template
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+import onnxruntime as rt
 
 __data_columns = None
 __model = None
 
 def predict_risk(age, sex, weight, height, alcohol_consumption, smoking, genetic_risk, physical_activity, diabetes, hypertension):
-    load_saved_artifiacts()
+    load_saved_artifacts()
     
     # Calculate BMI
     bmi = weight / (height**2)
     # Assign the values to the data list
     data = [age, sex, bmi, alcohol_consumption, smoking, genetic_risk, physical_activity, diabetes, hypertension]
 
-    # Define the feature names used during model training
-    feature_names = ['Age', 'Sex', 'BMI', 'AlcoholConsumption', 'Smoking', 'GeneticRisk', 'PhysicalActivity', 'Diabetes', 'Hypertension']
-
-    # Convert data to a dictionary
-    data_dict = dict(zip(feature_names, data))
+    # Prepare the input data
+    sess = rt.InferenceSession(__model.SerializeToString())
+    input_name = sess.get_inputs()[0].name
+    data = np.array([data], dtype=np.float32)
 
     # Make a prediction
-    probabilities = __model.predict_proba([data])
-
+    probabilities = sess.run(None, {input_name: data})
 
     # The returned probabilities are in the range [0, 1]. 
     # In the case of binary classification, `probabilities[0, 1]` will give you the probability of the positive class.
-    risk_percentage = probabilities[0, 1] * 100
+    print(probabilities)
+    risk_percentage = probabilities[1][0][1] * 100
     risk_percentage_string = f"{risk_percentage:.2f}%"
         
     return risk_percentage_string
 
-def load_saved_artifiacts():
+def load_saved_artifacts():
     print("Loading saved artifacts...start")
     global __data_columns
     global __model
 
+    with open('artifacts/log_model.pkl', 'rb') as f:
+        __skimodel = pickle.load(f)
+    initial_type = [('float_input', FloatTensorType([None, 9]))]  # assuming your input is a 1D array with 9 features
+    __model = convert_sklearn(__skimodel, initial_types=initial_type)
+
     with open('artifacts/columns.json', 'r') as f:
         __data_columns = json.load(f)['data_columns']
-
-    with open('artifacts/log_model.pkl', 'rb') as f:
-        __model = pickle.load(f)
 
     print("Loading saved artifacts...done")
 
